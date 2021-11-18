62d26f3747867cbe3270296c6565afb071f877a7
lucas-khattar <59715368+lucas-khattar@users.noreply.github.com>
2021-11-17 18:29:19 -0800

    11/17
    fix time errors, make it not military time

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -2,11 +2,17 @@ import React from "react";
 import classes from './SearchResult.module.css';
 
 const SearchResult = (props) => {
+    var date = new Date(1970, 0, 1);
+    var seconds = date.setSeconds(props.providerPublishTime);
+    var publishDate = new Date(seconds).toDateString();
+    var time = new Date(seconds).toLocaleTimeString('en-US');
+    var hour = new Date(seconds).getHours()-8;
+    var minute = new Date(seconds).getMinutes();
     console.log(props);
     return (
         <article className={classes.Result}>
             <h1>Title: {props.title}</h1>
-            <h3>Author: {props.publisher} ({props.providerPublishTime})</h3>
+            <h3>Author: {props.publisher} ({publishDate}, {hour}:{minute})</h3>
 
             <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
         </article>
